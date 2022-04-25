635d3eefe2461f5fc84887dae4d23ba751fe075c
limbers may <limbermay1@gmail.com>
2022-04-25 14:03:21 -0500

    We add the image class for to show the miniature of the image

--- views/main_vindow_view.py
+++ views/main_vindow_view.py
@@ -18,25 +18,14 @@ class MainWindowView(Frame):
 
         # Parts of the window
         self.image_display_roll = ImageDisplayRoll(self)
-        self.image_display = ImageDisplay(self)
+        self.image_display = ImageDisplayComplete(self)
 
         # Layout of the window
         self.image_display_roll.grid(row=0, column=0)
         self.image_display.grid(row=0, column=1)
 
 
-class ImageDisplayRoll(Frame):
-    def __init__(self, master):
-        Frame.__init__(self, master)
-
-        self.config(
-            width=300,
-            height=500,
-            bg="brown",
-        )
-
-
-class ImageDisplay(Frame):
+class ImageDisplayComplete(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
 
@@ -44,4 +33,34 @@ class ImageDisplay(Frame):
             width=500,
             height=500,
             bg="darkred",
+        )
+
+
+class ImageDisplayRoll(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+
+        self.config(
+            width=200,
+            height=500,
+            bg="brown",
+        )
+
+        self.grid_propagate(False)
+
+        # Parts of the window
+        self.image = Image(self)
+
+        # Layout of the window
+        self.image.grid(row=0, column=0)
+
+
+class Image(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+
+        self.config(
+            width=200,
+            height=150,
+            bg="green",
         )
