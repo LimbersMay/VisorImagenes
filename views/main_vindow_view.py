0d371b6d169f38690aee9af1ae555229bcc7cdc5
limbers may <limbermay1@gmail.com>
2022-04-25 13:57:11 -0500

    We add the image display for to display the image

--- views/main_vindow_view.py
+++ views/main_vindow_view.py
@@ -17,10 +17,12 @@ class MainWindowView(Frame):
         self.grid_propagate(False)
 
         # Parts of the window
-        self.image_display = ImageDisplayRoll(self)
+        self.image_display_roll = ImageDisplayRoll(self)
+        self.image_display = ImageDisplay(self)
 
         # Layout of the window
-        self.image_display.grid(row=0, column=0)
+        self.image_display_roll.grid(row=0, column=0)
+        self.image_display.grid(row=0, column=1)
 
 
 class ImageDisplayRoll(Frame):
@@ -31,4 +33,15 @@ class ImageDisplayRoll(Frame):
             width=300,
             height=500,
             bg="brown",
+        )
+
+
+class ImageDisplay(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+
+        self.config(
+            width=500,
+            height=500,
+            bg="darkred",
         )
